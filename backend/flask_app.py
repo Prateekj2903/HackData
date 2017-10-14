7be5ec2aaf48ba8b70c1a22cd309a288227bb2cd
Prateekj2903 <prateek.jain2903>
2017-10-14 20:45:31 +0530

    Updated Models

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -6,92 +6,55 @@ import json
 import os
 import keras
 import numpy as np
-from keras.models import load_model
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Conv2D, MaxPooling2D, Flatten
-# from PIL import Image
 from pyfcm import FCMNotification
-import matplotlib.pyplot as plt
 import cv2
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-val=""
-i=0
 @app.route('/')
 @app.route('/image', methods=['GET', 'POST'])
 def imageFunction():
     if request.method == 'GET':
         return "getting data"
     elif request.method == 'POST':
-        #data=request.form.getlist('[]')
         data=request.get_json(force=True)
         newFile=data['imageFile']
+        
         i = newFile[0]
         newFile = newFile[1:]
-        # print(newFile)
-        # newFile=data[10:]
-        # newFile= newFile.replace('%2F','/')
-        # newFile = newFile.replace('%0A','')
-        # newFile = newFile.replace('%2B','+')
-        # newFile = newFile.replace('%3D&','=')
-        # print(newFile)
-
+        
         imgdata = base64.b64decode(newFile)
         
-        
+
         filename = 'some_image.png'
         with open(filename, 'wb') as f:
             f.write(imgdata)
 
         img = cv2.imread('some_image.png', 0)
+        img = cv2.bitwise_not(img)
         
-        # plt.imshow(img, cmap='gray')
-        
+        for j in range(4):
+            img = cv2.pyrDown(img)
 
-
-
-        if i == 0:
-            img = cv2.resize(img, (28, 28))
-            resized = img.reshape((1,28,28,1))
-            model = Sequential()
-            model.add(Conv2D(64, kernel_size=(3, 3), input_shape = (28, 28, 1)))
-            model.add(Activation('relu'))
-            model.add(MaxPooling2D(pool_size=(2, 2)))
-            model.add(Dropout(0.25))
-            model.add(Conv2D(64, kernel_size=(3, 3)))
-            model.add(Activation('relu'))
-            model.add(Flatten())
-            model.add(Dense(256))
-            model.add(Activation('relu'))
-            model.add(Dropout(0.25))
-            model.add(Dense(10))
-            model.add(Activation('softmax'))
-            # model.summary()
-
-            model.load_weights('weights.h5')
-
-            pred = model.predict(resized)
-        # print (pred)
+        if i == '0':
+            img = cv2.pyrDown(img)    	
+            img = cv2.resize(img, (28,28))
+            model = keras.models.load_model('MNIST_Model.h5')
+            pred = model.predict_proba(img.reshape(1,28,28,1))
             val=np.argmax(pred)
-            print val
-        # print (val)
-        # print ("Got Image")
-        # plt.show()
+            print(val)
 
         else:
             img = cv2.resize(img, (64, 64))
-            resized = img.reshape((1,64,64,1))
-            model = load_model('model-chars.h5')
-
-            pred = model.predict(resized)
+            model = keras.models.load_model('75k_Model.h5')
+            pred = model.predict_proba(img.reshape(1,64,64,1))
             val = np.argmax(pred)
-            print val    
+            print(val)    
                     
-        testNotif()
+        testNotif(i, val)
         return "Success"
 
-def testNotif():
+def testNotif(i, val):
     push_service = FCMNotification(api_key="AAAAoroxYPI:APA91bHpnCDdaqNnCPLDPJ83v_RypNglyzBZmplMn_Qay_XCYBlCDUUTgG9pxwKL0xmgUA2GO1i-BZzBVUZ93X6Dj7cCHW7V-QfRigJRN7ZBgO9sphDW6czbc_rHVL0M8Z3RURcrH-Qi")
     registration_id = "ea8cVELQ8m8:APA91bFHNItw5DrXPJ3us7RST_fl_Sv38nGA4Q0YF0d1ryn5j3U8mk6rxEKR7F3ux9sd3-cYdF29gBRGHeNopM24KYKAx_wVkeXJNjzqbHh7wlgGLbqTRWml9l7Om6aYDWRd9y8WH_xc"
     data_message = {
@@ -99,7 +62,7 @@ def testNotif():
         "type":i
         }
     result = push_service.notify_single_device(registration_id=registration_id, data_message=data_message)
-    print result
+    print(result)
     return "success"
 
 if __name__ == '__main__':
